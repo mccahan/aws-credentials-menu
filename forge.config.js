b74b48aea0ee0d6dc33d370cfa17171e84b41edd
Ryan McCahan <ryan@mccahan.net>
2023-09-06 10:21:08 -0600

    Sign macOS binary

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: 'src/icon'
+    icon: 'src/icon',
+    osxSign: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    },
   },
   publishers: [
     {
